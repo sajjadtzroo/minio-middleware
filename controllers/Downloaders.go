80c0bbe82074f7d5ac2906fc5e7ba674a21f3a46
Hasan Parasteh <hasanparasteh@gmail.com>
2024-09-07 16:45:13 +0330

    better error handling

--- controllers/Downloaders.go
+++ controllers/Downloaders.go
@@ -13,12 +13,21 @@ import (
 )
 
 func DownloadFile(ctx *fiber.Ctx) error {
-	reqPath := ctx.Path()
-	var spilitted []string = strings.Split(reqPath, "/")
+	reqPath := strings.Split(ctx.Path(), "/")
+	if len(reqPath) < 3 {
+		if reqPath[1] == "favicon.ico" {
+			return ctx.SendStatus(404)
+		}
 
-	bucket := spilitted[1]
-	spilitted = slices.Delete(spilitted, 0, 2)
-	path := strings.Join(spilitted, "/")
+		return ctx.Status(400).JSON(models.GenericResponse{
+			Result:  false,
+			Message: "Invalid path",
+		})
+	}
+
+	bucket := reqPath[1]
+	reqPath = slices.Delete(reqPath, 0, 2)
+	path := strings.Join(reqPath, "/")
 
 	log.Printf("choosed %s bucket and downloading file %s", bucket, path)
 
